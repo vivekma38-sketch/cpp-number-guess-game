f79be59b43feb324d553b98e5670ab31a3bfc35c
Vivek Maurya <vivekma38@gmail.com>
2026-01-06 16:30:54 +0530

    Delete chapter1 directory

--- chapter1/for_loop.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-   for (int i = 1; i <= 5; i++) { // loop from 1 to 5
-       cout << "Iteration " << i << endl; // output current iteration
-   }
-    return 0;
-             
-}
